ba752dc25a07c926a22029c54c01e14b93764921
Phil Gee <originalphil@gmail.com>
2026-01-13 14:58:10 +0000

    AEA-6067 Convert spinecore to snake case.

--- tests/spinecore/changelog_test.py
+++ tests/spinecore/changelog_test.py
@@ -1,7 +1,3 @@
-"""
-Created on 11 Feb 2014
-"""
-
 import copy
 import sys
 import unittest
@@ -26,208 +22,224 @@ class ChangeLogProcessorTest(unittest.TestCase):
     Tests for the ChangeLogProcessor
     """
 
-    def testGeneralLogEntry_Empty(self):
+    def test_general_log_entry_empty(self):
         """
         test producing a general log with empty inputs
         """
-        logOfChange = ChangeLogProcessor.logForGeneralUpdate(1)
+        logOfChange = ChangeLogProcessor.log_for_general_update(1)
         del logOfChange["Timestamp"]
 
-        _expectedLog = {}
-        _expectedLog["SCN"] = 1
-        _expectedLog["InternalID"] = None
-        _expectedLog["Source XSLT"] = None
-        _expectedLog["Response Parameters"] = {}
-        self.assertEqual(logOfChange, _expectedLog)
+        expected_log = {}
+        expected_log["SCN"] = 1
+        expected_log["InternalID"] = None
+        expected_log["Source XSLT"] = None
+        expected_log["Response Parameters"] = {}
+        self.assertEqual(logOfChange, expected_log)
 
-    def testPruningOfChangeLog(self):
+    def test_pruning_of_change_log(self):
         """
         Add a new entry into change log and show it is correctly pruned
         """
-        _expectedChangeLog = copy.copy(CHANGE_LOG_TO_PRUNE)
-
+        expected_change_log = copy.copy(CHANGE_LOG_TO_PRUNE)
         record = {}
         record["changeLog"] = copy.copy(CHANGE_LOG_TO_PRUNE)
 
-        newLog = {"SCN": 12, "InternalID": "INTERNALID"}
-        _newRecord = ChangeLogProcessor.updateChangeLog(record, newLog, "GUID9", 6)
-        _newChangeLog = _newRecord["changeLog"]
+        new_log = {"SCN": 12, "InternalID": "INTERNALID"}
+        new_record = ChangeLogProcessor.update_change_log(record, new_log, "GUID9", 6)
+        new_change_log = new_record["changeLog"]
 
-        del _expectedChangeLog["GUID1"]
-        del _expectedChangeLog["GUID2"]
-        del _expectedChangeLog["GUID3"]
-        del _expectedChangeLog["GUID5"]
-        _expectedChangeLog["GUID9"] = newLog
+        del expected_change_log["GUID1"]
+        del expected_change_log["GUID2"]
+        del expected_change_log["GUID3"]
+        del expected_change_log["GUID5"]
+        expected_change_log["GUID9"] = new_log
 
-        self.assertDictEqual(_newChangeLog, _expectedChangeLog)
+        self.assertDictEqual(new_change_log, expected_change_log)
 
-    def testNotPruningOfChangeLog(self):
+    def test_not_pruning_of_change_log(self):
         """
         Add a new entry into change log and show that when DO_NOT_PRUNE is used it does not prune
         """
-        _expectedChangeLog = copy.copy(CHANGE_LOG_TO_PRUNE)
-
+        expected_change_log = copy.copy(CHANGE_LOG_TO_PRUNE)
         record = {}
         record["changeLog"] = copy.copy(CHANGE_LOG_TO_PRUNE)
 
-        newLog = {"SCN": 12, "InternalID": "INTERNALID"}
-        _newRecord = ChangeLogProcessor.updateChangeLog(
-            record, newLog, "GUID9", ChangeLogProcessor.DO_NOT_PRUNE
+        new_log = {"SCN": 12, "InternalID": "INTERNALID"}
+        new_record = ChangeLogProcessor.update_change_log(
+            record, new_log, "GUID9", ChangeLogProcessor.DO_NOT_PRUNE
         )
-        _newChangeLog = _newRecord["changeLog"]
+        new_change_log = new_record["changeLog"]
 
-        _expectedChangeLog["GUID9"] = newLog
+        expected_change_log["GUID9"] = new_log
 
-        self.assertDictEqual(_newChangeLog, _expectedChangeLog)
+        self.assertDictEqual(new_change_log, expected_change_log)
 
-    def testHighestSCN(self):
+    def test_highest_scn(self):
         """
         test highest guid and scn returned
         """
-        (guid, scn) = ChangeLogProcessor.getHighestSCN(CHANGE_LOG_TO_PRUNE)
+        (guid, scn) = ChangeLogProcessor.get_highest_scn(CHANGE_LOG_TO_PRUNE)
         self.assertEqual(guid, "GUID8")
         self.assertEqual(scn, 10)
 
         record = {}
         record["changeLog"] = copy.copy(CHANGE_LOG_TO_PRUNE)
 
-        newLog = {"SCN": 12, "InternalID": "INTERNALID"}
-        _newRecord = ChangeLogProcessor.updateChangeLog(record, newLog, "GUID9", 6)
-        _newChangeLog = _newRecord["changeLog"]
+        new_log = {"SCN": 12, "InternalID": "INTERNALID"}
+        new_record = ChangeLogProcessor.update_change_log(record, new_log, "GUID9", 6)
+        new_change_log = new_record["changeLog"]
 
-        (guid, scn) = ChangeLogProcessor.getHighestSCN(_newChangeLog)
+        (guid, scn) = ChangeLogProcessor.get_highest_scn(new_change_log)
         self.assertEqual(guid, "GUID9")
         self.assertEqual(scn, 12)
 
-    def testGetSCN(self):
+    def test_get_scn(self):
         """
         test return of SCN from changeLog entry
         """
-        changeLogEntry = {"SCN": 1}
-        scn = ChangeLogProcessor.getSCN(changeLogEntry)
+        change_log_entry = {"SCN": 1}
+        scn = ChangeLogProcessor.get_scn(change_log_entry)
         self.assertEqual(scn, 1)
-        changeLogEntry = {"SCN": "1"}
-        scn = ChangeLogProcessor.getSCN(changeLogEntry)
+
+        change_log_entry = {"SCN": "1"}
+        scn = ChangeLogProcessor.get_scn(change_log_entry)
         self.assertEqual(scn, 1)
-        changeLogEntry = {}
-        scn = ChangeLogProcessor.getSCN(changeLogEntry)
+
+        change_log_entry = {}
+        scn = ChangeLogProcessor.get_scn(change_log_entry)
         self.assertEqual(scn, ChangeLogProcessor.INVALID_SCN)
-        changeLogEntry = {"SCN": sys.maxsize}
-        scn = ChangeLogProcessor.getSCN(changeLogEntry)
+
+        change_log_entry = {"SCN": sys.maxsize}
+        scn = ChangeLogProcessor.get_scn(change_log_entry)
         self.assertEqual(scn, sys.maxsize)
 
-    def testListSCNs(self):
+    def test_list_scns(self):
         """
         test the return of the list of SCNs present in a changeLog
         """
-        changeLog = {"ABCD": {"SCN": 1}, "EFGH": {"SCN": 2}, "IJKL": {"SCN": 3}}
-        scnList = sorted(ChangeLogProcessor.listSCNs(changeLog))
-        self.assertEqual(scnList, [1, 2, 3])
+        change_log = {"ABCD": {"SCN": 1}, "EFGH": {"SCN": 2}, "IJKL": {"SCN": 3}}
+        scn_list = sorted(ChangeLogProcessor.list_scns(change_log))
+        self.assertEqual(scn_list, [1, 2, 3])
 
-        changeLog = {}
-        scnList = ChangeLogProcessor.listSCNs(changeLog)
-        scnList.sort()
-        self.assertEqual(scnList, [])
+        change_log = {}
+        scn_list = ChangeLogProcessor.list_scns(change_log)
+        scn_list.sort()
+        self.assertEqual(scn_list, [])
 
-        changeLog = {"ABCD": {}}
-        scnList = ChangeLogProcessor.listSCNs(changeLog)
-        scnList.sort()
-        self.assertEqual(scnList, [ChangeLogProcessor.INVALID_SCN])
+        change_log = {"ABCD": {}}
+        scn_list = ChangeLogProcessor.list_scns(change_log)
+        scn_list.sort()
+        self.assertEqual(scn_list, [ChangeLogProcessor.INVALID_SCN])
 
-    def testGetMaxSCN(self):
+    def test_get_max_scn(self):
         """
         Test retrieval of the highest SCN from changeLog
         """
-        changeLog = {"ABCD": {"SCN": 1}, "IJKL": {"SCN": 3}, "EFGH": {"SCN": 2}}
-        highestSCN = ChangeLogProcessor.getMaxSCN(changeLog)
-        self.assertEqual(highestSCN, 3)
+        change_log = {"ABCD": {"SCN": 1}, "IJKL": {"SCN": 3}, "EFGH": {"SCN": 2}}
+        highest_scn = ChangeLogProcessor.get_max_scn(change_log)
+        self.assertEqual(highest_scn, 3)
 
-        changeLog = {"ABCD": {"SCN": 1}, "EFGH": {"SCN": 2}, "IJKL": {"SCN": 3}, "ZZZZ": {"SCN": 3}}
-        highestSCN = ChangeLogProcessor.getMaxSCN(changeLog)
-        self.assertEqual(highestSCN, 3)
+        change_log = {
+            "ABCD": {"SCN": 1},
+            "EFGH": {"SCN": 2},
+            "IJKL": {"SCN": 3},
+            "ZZZZ": {"SCN": 3},
+        }
+        highest_scn = ChangeLogProcessor.get_max_scn(change_log)
+        self.assertEqual(highest_scn, 3)
 
-        changeLog = {"ABCD": {}}
-        highestSCN = ChangeLogProcessor.getMaxSCN(changeLog)
-        self.assertEqual(highestSCN, ChangeLogProcessor.INVALID_SCN)
+        change_log = {"ABCD": {}}
+        highest_scn = ChangeLogProcessor.get_max_scn(change_log)
+        self.assertEqual(highest_scn, ChangeLogProcessor.INVALID_SCN)
 
-    def testGetAllGuidsForSCN(self):
+    def test_get_all_guids_for_scn(self):
         """
         test retrieval of list of GUIDS that are keys for changelog entries which have a particular SCN
         """
-        changeLog = {"ABCD": {"SCN": 1}, "EFGH": {"SCN": 2}, "IJKL": {"SCN": 3}, "ZZZZ": {"SCN": 3}}
-        guidList = sorted(ChangeLogProcessor.getAllGuidsForSCN(changeLog, 1))
-        self.assertEqual(guidList, ["ABCD"])
+        change_log = {
+            "ABCD": {"SCN": 1},
+            "EFGH": {"SCN": 2},
+            "IJKL": {"SCN": 3},
+            "ZZZZ": {"SCN": 3},
+        }
+        guid_list = sorted(ChangeLogProcessor.get_all_guids_for_scn(change_log, 1))
+        self.assertEqual(guid_list, ["ABCD"])
 
-        guidList = ChangeLogProcessor.getAllGuidsForSCN(changeLog, 3)
-        guidList.sort()
-        self.assertEqual(guidList, ["IJKL", "ZZZZ"])
+        guid_list = ChangeLogProcessor.get_all_guids_for_scn(change_log, 3)
+        guid_list.sort()
+        self.assertEqual(guid_list, ["IJKL", "ZZZZ"])
 
-        guidList = ChangeLogProcessor.getAllGuidsForSCN(changeLog, "3")
-        guidList.sort()
-        self.assertEqual(guidList, ["IJKL", "ZZZZ"])
+        guid_list = ChangeLogProcessor.get_all_guids_for_scn(change_log, "3")
+        guid_list.sort()
+        self.assertEqual(guid_list, ["IJKL", "ZZZZ"])
 
-        guidList = ChangeLogProcessor.getAllGuidsForSCN(changeLog, "7")
-        guidList.sort()
-        self.assertEqual(guidList, [])
+        guid_list = ChangeLogProcessor.get_all_guids_for_scn(change_log, "7")
+        guid_list.sort()
+        self.assertEqual(guid_list, [])
 
-    def testGetMaxSCNGuids(self):
+    def test_get_max_scn_guids(self):
         """
         test retrieval of all GUIDS that have the highest SCN in the changeLog entry
         """
         changeLog = {"ABCD": {"SCN": 1}, "IJKL": {"SCN": 3}, "EFGH": {"SCN": 2}}
-        guidList = sorted(ChangeLogProcessor.getMaxSCNGuids(changeLog))
+        guidList = sorted(ChangeLogProcessor.get_max_scn_guids(changeLog))
         self.assertEqual(guidList, ["IJKL"])
 
         changeLog = {"ABCD": {"SCN": 1}, "EFGH": {"SCN": 2}, "IJKL": {"SCN": 3}, "ZZZZ": {"SCN": 3}}
-        guidList = ChangeLogProcessor.getMaxSCNGuids(changeLog)
+        guidList = ChangeLogProcessor.get_max_scn_guids(changeLog)
         guidList.sort()
         self.assertEqual(guidList, ["IJKL", "ZZZZ"])
 
         changeLog = {"ABCD": {}, "EFGH": {}}
-        guidList = ChangeLogProcessor.getMaxSCNGuids(changeLog)
+        guidList = ChangeLogProcessor.get_max_scn_guids(changeLog)
         guidList.sort()
         self.assertEqual(guidList, ["ABCD", "EFGH"])
 
         changeLog = {"ABCD": {}, "EFGH": {}, "IJKL": {"SCN": 3}}
-        guidList = ChangeLogProcessor.getMaxSCNGuids(changeLog)
+        guidList = ChangeLogProcessor.get_max_scn_guids(changeLog)
         guidList.sort()
         self.assertEqual(guidList, ["IJKL"])
 
         changeLog = {}
-        guidList = ChangeLogProcessor.getMaxSCNGuids(changeLog)
+        guidList = ChangeLogProcessor.get_max_scn_guids(changeLog)
         self.assertEqual(guidList, [])
 
-    def testGetAllGuids(self):
+    def test_get_all_guids(self):
         """
         test getting the list of all GUID keys for a changeLog
         """
-        changeLog = {"ABCD": {"SCN": 1}, "EFGH": {"SCN": 2}, "IJKL": {"SCN": 3}, "ZZZZ": {"SCN": 3}}
-        guidList = sorted(ChangeLogProcessor.getAllGuids(changeLog))
-        self.assertEqual(guidList, ["ABCD", "EFGH", "IJKL", "ZZZZ"])
+        change_log = {
+            "ABCD": {"SCN": 1},
+            "EFGH": {"SCN": 2},
+            "IJKL": {"SCN": 3},
+            "ZZZZ": {"SCN": 3},
+        }
+        guid_list = sorted(ChangeLogProcessor.get_all_guids(change_log))
+        self.assertEqual(guid_list, ["ABCD", "EFGH", "IJKL", "ZZZZ"])
 
-        changeLog = {"ABCD": {}, "EFGH": {}}
-        guidList = ChangeLogProcessor.getAllGuids(changeLog)
-        guidList.sort()
-        self.assertEqual(guidList, ["ABCD", "EFGH"])
+        change_log = {"ABCD": {}, "EFGH": {}}
+        guid_list = ChangeLogProcessor.get_all_guids(change_log)
+        guid_list.sort()
+        self.assertEqual(guid_list, ["ABCD", "EFGH"])
 
-        changeLog = {}
-        guidList = ChangeLogProcessor.getAllGuids(changeLog)
-        self.assertEqual(guidList, [])
+        change_log = {}
+        guid_list = ChangeLogProcessor.get_all_guids(change_log)
+        self.assertEqual(guid_list, [])
 
-    def testSettingInitialChangeLogOnDataMigration(self):
+    def test_setting_initial_change_log_on_data_migration(self):
         """
         Set an initial change log onto a record which does not have one
         """
         record = {}
-        internalID = "INTERNALID"
-        reasonGUID = "DataMigration"
-        ChangeLogProcessor.setInitialChangeLog(record, internalID, reasonGUID)
+        internal_id = "INTERNALID"
+        reason_guid = "DataMigration"
+        ChangeLogProcessor.set_initial_change_log(record, internal_id, reason_guid)
 
-        _changeLog = record[ChangeLogProcessor.RECORD_CHANGELOG_REF]
-        del _changeLog["DataMigration"]["Timestamp"]
+        change_log = record[ChangeLogProcessor.RECORD_CHANGELOG_REF]
+        del change_log["DataMigration"]["Timestamp"]
 
         self.assertDictEqual(
-            _changeLog,
+            change_log,
             {
                 "DataMigration": {
                     "SCN": 1,
@@ -270,15 +282,15 @@ class PrescriptionChangeLogProcessorTest(unittest.TestCase):
     Tests for the ChangeLogProcessor
     """
 
-    def testPrunePrescriptionChangeLog(self):
+    def test_prune_prescription_change_log(self):
         """
         Prune the record as expected
         """
-        _changeLog = copy.copy(PR_CHANGE_LOG_TO_PRUNE)
+        change_log = copy.copy(PR_CHANGE_LOG_TO_PRUNE)
 
-        PrescriptionsChangeLogProcessor.pruneChangeLog(_changeLog, 80)
+        PrescriptionsChangeLogProcessor.prune_change_log(change_log, 80)
 
-        _presentGUIDs = [
+        present_guids = [
             "GUID1",
             "GUID2",
             "GUID3",
@@ -294,27 +306,26 @@ class PrescriptionChangeLogProcessorTest(unittest.TestCase):
             "GUIDZ",
         ]
 
-        for guid in _presentGUIDs:
-            self.assertIn(guid, list(_changeLog.keys()))
+        for guid in present_guids:
+            self.assertIn(guid, list(change_log.keys()))
 
-        self.assertEqual(len(_presentGUIDs), len(list(_changeLog.keys())))
+        self.assertEqual(len(present_guids), len(list(change_log.keys())))
 
-    def testPrunePrescriptionChangeLog_HghPrunePoint(self):
+    def test_prune_prescription_change_log_high_prune_point(self):
         """
         Increase the prune point, and confirm now no pruning
         """
-        _changeLog = copy.copy(PR_CHANGE_LOG_TO_PRUNE)
+        change_log = copy.copy(PR_CHANGE_LOG_TO_PRUNE)
+        PrescriptionsChangeLogProcessor.prune_change_log(change_log, 180)
+        self.assertDictEqual(change_log, PR_CHANGE_LOG_TO_PRUNE)
 
-        PrescriptionsChangeLogProcessor.pruneChangeLog(_changeLog, 180)
-        self.assertDictEqual(_changeLog, PR_CHANGE_LOG_TO_PRUNE)
-
-    def testUnprunableChangeLog(self):
+    def test_unprunable_change_log(self):
         """
         Make the change log unprunable below the prune point
         """
-        _changeLog = copy.copy(PR_CHANGE_LOG_TO_PRUNE)
+        change_log = copy.copy(PR_CHANGE_LOG_TO_PRUNE)
         for scn in range(100, 200):
-            _changeLog["GUID" + str(scn)] = {"SCN": scn, "interactionID": "PORX_IN090101UK09"}
+            change_log["GUID" + str(scn)] = {"SCN": scn, "interactionID": "PORX_IN090101UK09"}
 
         with self.assertRaises(EpsSystemError):
-            PrescriptionsChangeLogProcessor.pruneChangeLog(_changeLog, 50)
+            PrescriptionsChangeLogProcessor.prune_change_log(change_log, 50)
